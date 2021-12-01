ab5423b0f268bf6423f584f83a80e8c64c0d1450
Sergey <skiv-2@mail.ru>
2021-12-01 19:36:16 +0300

    did 4 HM easy and normal + comment

--- hw04_easy.py
+++ hw04_easy.py
@@ -3,7 +3,10 @@
 # т.е функция ничего не возвращает, а выводит на консоль ответ самостоятельно
 # Предполагается, что 1км = 1,609 мили
 def convert(km):
+    ml = 1.609
+    miles = km / ml
     print(miles)
+a = convert(100)
 
 # Задание-2:
 # Напишите функцию, округляющую полученное произвольное десятичное число
@@ -11,9 +14,12 @@ def convert(km):
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 def my_round(number, ndigits):
-    pass
-
-
+    number = number * (10 ** ndigits)
+    if float(number) - int(number) > 0.5:
+         number = number // 1 + 1
+    else:
+         number = number // 1
+    return number / (10 ** ndigits)
 print(my_round(2.1234567, 5))
 print(my_round(2.1999967, 5))
 print(my_round(2.9999967, 5))
@@ -27,9 +33,16 @@ print(my_round(2.9999967, 5))
 # ибо False (если счастливый и несчастливый соответственно)
 
 def lucky_ticket(ticket_number):
-    pass
-
-
+    if (len(str(ticket_number)) != 6) or (type(ticket_number) is not int):
+        return 'Некорректный номер билета'
+    else:
+        ticket_number1 = list(str(ticket_number))
+        sum1 = int(ticket_number1[0]) + int(ticket_number1[1]) + int(ticket_number1[2])
+        sum2 = int(ticket_number1[3]) + int(ticket_number1[4]) + int(ticket_number1[5])
+        if sum1 == sum2:
+            return 'Билет %s счастливый' % ticket_number
+        else:
+            return 'Билет %s несчастливый' % ticket_number
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
 print(lucky_ticket(436751))
